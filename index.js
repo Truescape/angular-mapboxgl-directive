7df19de8228da5de133037fb4a393f4eab6c6fdd
Drian Naude <drian.naude@truescape.com>
2017-02-01 11:39:13 +1300

    broken requires into src

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
-require('./dist/angular-mapboxgl-directive');
+// "src/directives/mapboxgl.js",
+// "src/directives/mapboxglCompare.js",
+// "src/factories/*.js", "src/**/*.js"
+
+// Directives
+require('./src/directives/mapboxgl');
+require('./src/directives/mapboxglCompare');
+// factories
+require('./src/factories/mapboxglEventsUtils');
+require('./src/factories/mapboxglImageUtils');
+require('./src/factories/mapboxglLayerUtils');
+require('./src/factories/mapboxglMapsData');
+require('./src/factories/mapboxglMarkerUtils');
+require('./src/factories/mapboxglPopupUtils');
+require('./src/factories/mapboxglSourceUtils');
+require('./src/factories/mapboxglUtils');
+require('./src/factories/mapboxglVideoUtils');
+// constants
+require('./src/constants/mapboxglConstants');
+// Other directives
+require('./src/directives/glBearing');
+require('./src/directives/glCenter');
+require('./src/directives/glClasses');
+require('./src/directives/glControls');
+require('./src/directives/glFilter');
+require('./src/directives/glHandlers');
+require('./src/directives/glImage');
+require('./src/directives/glInteractive');
+require('./src/directives/glLayers');
+require('./src/directives/glLights');
+require('./src/directives/glMarkers');
+require('./src/directives/glMaxBounds');
+require('./src/directives/glMaxZoom');
+require('./src/directives/glMinZoom');
+require('./src/directives/glPitch');
+require('./src/directives/glPopups');
+require('./src/directives/glSources');
+require('./src/directives/glStyle');
+require('./src/directives/Video');
+require('./src/directives/glZoom');
+
+
 module.exports = 'mapboxgl-directive';
